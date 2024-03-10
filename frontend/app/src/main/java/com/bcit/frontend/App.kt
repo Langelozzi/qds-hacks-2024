e175e7ee1a67fc5ecdf7f0676b3383cd6d9f454c
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2024-03-10 01:25:32 -0800

    Add background to app

--- frontend/app/src/main/java/com/bcit/frontend/App.kt
+++ frontend/app/src/main/java/com/bcit/frontend/App.kt
@@ -1,10 +1,8 @@
 package com.bcit.frontend
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -13,7 +11,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -50,71 +47,72 @@ fun App() {
         )
     }
 
-    val incompleteTasks = remember {
+    val unsortedTasks = remember {
         mutableStateListOf(
             Task("Lab 1", "CPSC", TaskType.LAB, 0.05, "2022-10-10", 2, R.drawable.stats2),
             Task("quiz 1", "Stats", TaskType.LAB, 0.05, "2022-10-10", 2, R.drawable.algolab3),
-            Task("midterm 1", "Maths", TaskType.LAB, 0.05, "2022-10-10",2, R.drawable.ooplab2),
-            Task("assignment 1", "AIML", TaskType.LAB, 0.05, "2022-10-10",2, R.drawable.ooplab1),
+            Task("midterm 1", "Maths", TaskType.LAB, 0.05, "2022-10-10", 2, R.drawable.ooplab2),
+            Task("assignment 1", "AIML", TaskType.LAB, 0.05, "2022-10-10", 2, R.drawable.ooplab1),
             Task("Lab 1", "CPSC", TaskType.LAB, 0.05, "2022-10-10", 2, R.drawable.statslab2),
             Task("Lab 1", "Stats", TaskType.LAB, 0.05, "2022-10-10", 2, R.drawable.algolab3),
         )
     }
-    var displayedTasksIndexes = remember{
-        mutableStateListOf(0,1)
-    }
-    var displayedTasks = remember {
-        mutableStateListOf(incompleteTasks[displayedTasksIndexes[0]], incompleteTasks[displayedTasksIndexes[1]])
+
+    val sortedTasks = remember {
+        mutableStateListOf<Task>()
     }
 
     val removeTask: (Task) -> Unit = { task ->
         // this function should remove the passed task from the list
     }
-    val taskSwipped: (Task) -> Unit = { task ->
-        displayedTasksIndexes[0] += 1
-        displayedTasksIndexes[1] += 1
-        displayedTasks[0] = incompleteTasks[displayedTasksIndexes[0]]
-        displayedTasks[1] = incompleteTasks[displayedTasksIndexes[1]]
+
+    val addNewTask: (Task) -> Unit = { task ->
+        unsortedTasks.add(task)
         // you should be able to see when this is swiped it gets logged
     }
-    val addTask: (Task) -> Unit = { task ->
-        incompleteTasks.add(task)
-        // you should be able to see when this is swiped it gets logged
+
+    val addSortedTasks: (Array<Task>) -> Unit = { tasks ->
+        sortedTasks.addAll(tasks)
+        unsortedTasks.removeAll(tasks.toSet())
     }
 
     val completeTask: (Task) -> Unit = { task ->
-        incompleteTasks.remove(task)
+        unsortedTasks.remove(task)
         completedTasks.add(task)
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    )
-    {
 
-        NavBar(navController, activePage, setActivePage)
-
-        Box(
+    Box {
+        BackgroundImage()
+        Column(
             modifier = Modifier
+                .fillMaxSize()
         )
         {
-            NavHost(
-                navController = navController,
-                startDestination = "home"
-            ) {
-                composable(route = "VS") {
-                    VsPage(displayedTasks, taskSwipped)
+
+            NavBar(navController, activePage, setActivePage)
+
+            Box(
+                modifier = Modifier
+            )
+            {
+                NavHost(
+                    navController = navController,
+                    startDestination = "home"
+                ) {
+                    composable(route = "VS") {
+                        VsPage(unsortedTasks.toList(), addSortedTasks)
+                    }
+                    composable(route = "Home") {
+                        HomePage(unsortedTasks, addNewTask, completeTask)
+                    }
+                    composable(route = "Complete") {
+                        CompletedPage(completedTasks)
+                    }
                 }
-                composable(route = "Home") {
-                    HomePage(incompleteTasks, addTask, completeTask)
-                }
-                composable(route = "Complete") {
-                    CompletedPage(completedTasks)
-                }
+
             }
 
         }
-
     }
 }
